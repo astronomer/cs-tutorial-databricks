b3fb207ac45c37bf35d12688ad83dacb7aec32ef
Chris Hronek <pohek321@gmail.com>
2021-11-05 12:09:51 -0600

    Update with taskgroups

--- dags/databricks_example_dag.py
+++ dags/databricks_example_dag.py
@@ -1,9 +1,11 @@
 from airflow import DAG
 from airflow.operators.dummy import DummyOperator
 from airflow.operators.python import PythonOperator
+from airflow.utils.task_group import TaskGroup
 from airflow.providers.databricks.operators.databricks import DatabricksRunNowOperator, DatabricksSubmitRunOperator
 from datetime import datetime
 from include.databricks_tools import DatabricksUtil
+from airflow.providers.databricks.hooks.databricks import DatabricksHook
 
 '''
 Notes
@@ -53,158 +55,146 @@ with DAG('databricks_example_dag',
         task_id='finish'
     )
 
-    #runs a job that is already assigned to an existing all purpose cluster
-    opr_run_now = DatabricksRunNowOperator(
-        task_id="databricks_run_now",
-        databricks_conn_id="databricks_default",
-        job_id=str(job_id)
-    )
+    with TaskGroup(group_id="operators") as g1:
+        #runs a job that is already assigned to an existing all purpose cluster
+        opr_run_now = DatabricksRunNowOperator(
+            task_id="databricks_run_now",
+            databricks_conn_id="databricks_default",
+            job_id=str(job_id)
+        )
 
-    #spins up a new cluster and runs a job - to see the cluster look at the "Job Clusters" tab in Databricks UI
-    opr_submit_run = DatabricksSubmitRunOperator(
-        task_id='databricks_submit_run',
-        json={
-            'new_cluster': new_cluster,
-            'notebook_task': {
-                'notebook_path': '/Users/chronek@astronomer.io/hello_world_2'
-            }
-        }
-    )
-
-    #runs a job that is already assigned to an existing all purpose cluster
-    python_run_now = PythonOperator(
-        task_id="python_run_now",
-        python_callable=DatabricksUtil().run_now,
-        op_kwargs={
-            "job_id": str(job_id)
-        }
-    )
-
-    # spins up a new cluster and runs a job - to see the cluster look at the "Job Clusters" tab in Databricks UI
-    python_submit_run = PythonOperator(
-        task_id="python_submit_run",
-        python_callable=DatabricksUtil().submit_run,
-        op_kwargs={
-            "json": {
+        #spins up a new cluster and runs a job - to see the cluster look at the "Job Clusters" tab in Databricks UI
+        opr_submit_run = DatabricksSubmitRunOperator(
+            task_id='databricks_submit_run',
+            json={
                 'new_cluster': new_cluster,
                 'notebook_task': {
                     'notebook_path': '/Users/chronek@astronomer.io/hello_world_2'
                 }
             }
-        }
-    )
+        )
 
-    #returns url for the specified run given the run_id (see xcom)
-    python_get_run_page_url = PythonOperator(
-        task_id="python_get_run_page_url",
-        python_callable=DatabricksUtil().get_run_page_url,
-        op_kwargs={
-            "run_id": str(run_id)
-        }
-    )
+    with TaskGroup(group_id="hook_methods") as g2:
+        with TaskGroup(group_id="job_interaction") as g3:
+            #runs a job that is already assigned to an existing all purpose cluster
+            python_run_now = PythonOperator(
+                task_id="python_run_now",
+                python_callable=DatabricksUtil().run_now,
+                op_kwargs={
+                    "job_id": str(job_id)
+                }
+            )
 
-    #returns job_id given a run_id (see xcom)
-    python_get_job_id = PythonOperator(
-        task_id="python_get_job_id",
-        python_callable=DatabricksUtil().get_job_id,
-        op_kwargs={
-            "run_id": str(run_id)
-        }
-    )
-
-    #I think this is broken opened a bug here https://github.com/apache/airflow/issues/19357
-    python_get_run_state = PythonOperator(
-        task_id="python_get_run_state",
-        python_callable=DatabricksUtil().get_run_state,
-        op_kwargs={
-            "run_id": str(run_id)
-        }
-    )
-
-    #cancels a specified run for a job given a run id
-    python_cancel_run = PythonOperator(
-        task_id="python_cancel_run",
-        python_callable=DatabricksUtil().cancel_run,
-        op_kwargs={
-            "run_id": str(run_id)
-        }
-    )
-
-    #starts a terminated cluster
-    python_start_cluster = PythonOperator(
-        task_id="python_start_cluster",
-        python_callable=DatabricksUtil().start_cluster,
-        op_kwargs={
-            "cluster_id": cluster_id
-        }
-    )
-
-    #restarts a cluster
-    python_restart_cluster = PythonOperator(
-        task_id="python_restart_cluster",
-        python_callable=DatabricksUtil().restart_cluster,
-        op_kwargs={
-            "cluster_id": cluster_id
-        }
-    )
-
-    #terminates a cluster
-    python_terminate_cluster = PythonOperator(
-        task_id="python_terminate_cluster",
-        python_callable=DatabricksUtil().terminate_cluster,
-        op_kwargs={
-            "cluster_id": cluster_id
-        }
-    )
-
-    #install package dependencies on a cluster
-    python_install = PythonOperator(
-        task_id="python_install",
-        python_callable=DatabricksUtil().install,
-        op_kwargs={
-            "json": {
-                "cluster_id": cluster_id,
-                "libraries": [{
-                    "pypi": {
-                        "package": "requests"
-                    }
-                }]
-            }
-        }
-    )
-
-    #uninstall package dependencies on a cluster
-    python_uninstall = PythonOperator(
-        task_id="python_uninstall",
-        python_callable=DatabricksUtil().uninstall,
-        op_kwargs={
-            "json": {
-                "cluster_id": cluster_id,
-                "libraries": [{
-                        "pypi": {
-                            "package": "requests"
+            # spins up a new cluster and runs a job - to see the cluster look at the "Job Clusters" tab in Databricks UI
+            python_submit_run = PythonOperator(
+                task_id="python_submit_run",
+                python_callable=DatabricksUtil().submit_run,
+                op_kwargs={
+                    "json": {
+                        'new_cluster': new_cluster,
+                        'notebook_task': {
+                            'notebook_path': '/Users/chronek@astronomer.io/hello_world_2'
                         }
-                    }]
-            }
-        }
-    )
+                    }
+                }
+            )
 
-    hook_actions = DummyOperator(task_id="hook_actions")
-    hook_action_tasks = [
-        python_run_now,
-        python_submit_run,
-        python_get_run_page_url,
-        python_get_job_id,
-        python_get_run_state,
-        python_cancel_run,
-        python_start_cluster,
-        python_restart_cluster,
-        python_terminate_cluster,
-        python_install,
-        python_uninstall
-    ]
-    databricks_operators = DummyOperator(task_id="databricks_operators")
-    databricks_operator_tasks = [opr_run_now, opr_submit_run]
+            #returns url for the specified run given the run_id (see xcom)
+            python_get_run_page_url = PythonOperator(
+                task_id="python_get_run_page_url",
+                python_callable=DatabricksUtil().get_run_page_url,
+                op_kwargs={
+                    "run_id": str(run_id)
+                }
+            )
 
-    start >> databricks_operators >> databricks_operator_tasks >> finish
-    start >> hook_actions >> hook_action_tasks >> finish
+            #returns job_id given a run_id (see xcom)
+            python_get_job_id = PythonOperator(
+                task_id="python_get_job_id",
+                python_callable=DatabricksUtil().get_job_id,
+                op_kwargs={
+                    "run_id": str(run_id)
+                }
+            )
+
+            #I think this is broken opened a bug here https://github.com/apache/airflow/issues/19357
+            python_get_run_state = PythonOperator(
+                task_id="python_get_run_state",
+                python_callable=DatabricksUtil().get_run_state,
+                op_kwargs={
+                    "run_id": str(run_id)
+                }
+            )
+
+            #cancels a specified run for a job given a run id
+            python_cancel_run = PythonOperator(
+                task_id="python_cancel_run",
+                python_callable=DatabricksUtil().cancel_run,
+                op_kwargs={
+                    "run_id": str(run_id)
+                }
+            )
+
+        with TaskGroup("clusters") as g4:
+            #starts a terminated cluster
+            python_start_cluster = PythonOperator(
+                task_id="python_start_cluster",
+                python_callable=DatabricksUtil().start_cluster,
+                op_kwargs={
+                    "cluster_id": cluster_id
+                }
+            )
+
+            #restarts a cluster
+            python_restart_cluster = PythonOperator(
+                task_id="python_restart_cluster",
+                python_callable=DatabricksUtil().restart_cluster,
+                op_kwargs={
+                    "cluster_id": cluster_id
+                }
+            )
+
+            #terminates a cluster
+            python_terminate_cluster = PythonOperator(
+                task_id="python_terminate_cluster",
+                python_callable=DatabricksUtil().terminate_cluster,
+                op_kwargs={
+                    "cluster_id": cluster_id
+                }
+            )
+
+            #install package dependencies on a cluster
+            python_install = PythonOperator(
+                task_id="python_install",
+                python_callable=DatabricksUtil().install,
+                op_kwargs={
+                    "json": {
+                        "cluster_id": cluster_id,
+                        "libraries": [{
+                            "pypi": {
+                                "package": "requests"
+                            }
+                        }]
+                    }
+                }
+            )
+
+            #uninstall package dependencies on a cluster
+            python_uninstall = PythonOperator(
+                task_id="python_uninstall",
+                python_callable=DatabricksUtil().uninstall,
+                op_kwargs={
+                    "json": {
+                        "cluster_id": cluster_id,
+                        "libraries": [{
+                                "pypi": {
+                                    "package": "requests"
+                                }
+                            }]
+                    }
+                }
+            )
+
+
+    start >> g1 >> finish
+    start >> g2 >> finish
